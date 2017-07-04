5507083b7ef45d11ebb949c155e6087a622cc8cc
Amourie Fourie <amourie@gmail.com>
2017-07-04 20:59:12 +1000

    Add new class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace TeamPowered
 {
-    class Program
+    class Person
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
